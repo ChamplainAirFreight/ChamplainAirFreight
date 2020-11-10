cceda1cc5635fa682ac08d1de401786dfa7666bd
ADockan <Andrew.Dockan@mymail.champlain.edu>
2020-11-10 17:21:43 -0600

    updating personal branch to match master

--- ChamplainAirFreight/src/main/java/edu/cco/ChamplainAirFreight/ClientsPage.java
+++ ChamplainAirFreight/src/main/java/edu/cco/ChamplainAirFreight/ClientsPage.java
@@ -1,10 +1,16 @@
 package edu.cco.ChamplainAirFreight;
 
 import java.util.Arrays;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -22,18 +28,36 @@ import javafx.scene.text.Text;
 //Imports
 //Begin Subclass ClientsPage
 public class ClientsPage {
+	static TextArea texReaOne = new TextArea();
 
     // classes 
     Styles s = new Styles();
+    DBViewAllClient viewAllClient = new DBViewAllClient();  //view all client information 
+    
+	// Client address labels
+	static Label lbName = new Label("Client Name");
+	static Label lbAddress = new Label("Address");
+	static Label lbCity = new Label("City");
+	static Label lbState = new Label("State");
+	static ComboBox<String> cbState;
+	static Label lbZip = new Label("Zip");
+
+	static TextField txName;
+	static TextField txAddress;
+	static TextField txCity;
+	static TextField txState;
+	static TextField txZip;
 
     //passed border pane from CAF. 
     BorderPane bPane = new BorderPane();
+    
 
     /**
      * constructor pulls the border pane from CAF
      */
     ClientsPage(BorderPane bp) {
         bPane = bp;
+		
     }
 
     /**
@@ -42,7 +66,7 @@ public class ClientsPage {
      */
     public VBox getPane() {
         VBox vclients = new VBox();
-        vclients.getChildren().add(clients());
+        vclients.getChildren().addAll(clients());
         return vclients;
     }
 
@@ -52,7 +76,7 @@ public class ClientsPage {
      */
     private BorderPane clients() {
         BorderPane box = new BorderPane();
-
+        
         //create a title pane for the top
         VBox titleBox = new VBox();
         titleBox.setAlignment(Pos.CENTER);
@@ -70,13 +94,20 @@ public class ClientsPage {
         //add titles to titlebox. 
         titleBox.getChildren().addAll(title, instruct);
 
+        //creating center box to add client information - Pierre
+        HBox centerBox = new HBox();
+        centerBox.setAlignment(Pos.CENTER_LEFT);
+        centerBox.setMinHeight(300);
+        centerBox.setStyle("-fx-background-color: white");
+        centerBox.getChildren().addAll();
+        
         //create button HBox:
         HBox buttonBox = new HBox();
         buttonBox.setAlignment(Pos.CENTER);
         buttonBox.setSpacing(20);
 
         //make buttons
-        Button btnView = new Button("View Client");
+        Button btnView = new Button("View");
         Button btnAdd = new Button("Add Client");
         Button btnEdit = new Button("Edit Client");
         Button btnDelete = new Button("Delete Client");
@@ -94,7 +125,7 @@ public class ClientsPage {
         //Exit button style
         btnExit.setStyle(s.redEntryButtons);
         btnExit.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 14));
-        btnExit.setMinHeight(30);
+        btnExit.setMinHeight(0);
 
         //add buttons to button HBox
         buttonBox.getChildren().addAll(btnView, btnAdd, btnEdit, btnDelete, btnEnter,
@@ -102,18 +133,19 @@ public class ClientsPage {
 
         //add title, center, and buttons to clients pane:
         box.setTop(titleBox);
-        box.setCenter(viewClient()); //call a method to show db of clients  
+        box.setCenter(centerBox); //call a method to show db of clients  
         box.setBottom(buttonBox);
 
         //add actionables to change the setCenter based on button responses:
         btnView.setOnAction(e -> {
-            box.setCenter(viewClient());
+        	 box.setCenter(getViewLBs());//(getViewLBs()); 
+        //	getClientInfolb(), getClientInfotx()
         });
         btnAdd.setOnAction(e -> {
-            //box.setCenter(addClient()); 
+            box.setCenter(getClientLBs()); 
         });
         btnEdit.setOnAction(e -> {
-
+        	box.setCenter(getClientLBs()); 
         });
         btnDelete.setOnAction(e -> {
 
@@ -127,7 +159,7 @@ public class ClientsPage {
         btnExit.setOnAction(e -> {
             //clear whatever actions doing
             //return to just the viewClient page
-            box.setCenter(viewClient());
+            box.setCenter(centerBox);
         });
 
         return box;
@@ -137,12 +169,148 @@ public class ClientsPage {
      * will connect to db to view client information 
      * @return 
      */
-    private ScrollPane viewClient() {
+    private ScrollPane addClient() {
         ScrollPane chart = new ScrollPane();
-        chart.setMinHeight(360);
+        chart.setMinHeight(560);
         chart.setMaxHeight(360);
 
         return chart;
     }
-
+    /**
+     * This is a function used to display the header section and the TextArea section for the result.
+     * @return
+     */
+    private HBox getClientLBs() {
+    	HBox hboxlb = new HBox();
+    	hboxlb.setMinHeight(300);
+    	hboxlb.setStyle("-fx-background-color: white");
+    	hboxlb.setAlignment(Pos.CENTER_LEFT);
+    	hboxlb.getChildren().addAll(getClientInfolb(),getClientInfotx());
+    	return hboxlb;
+    }   
+    /*
+     * This function will display the client information for the client page
+     */
+    private VBox getClientInfolb() {
+    	VBox vboxi = new VBox();
+        GridPane gPane = new GridPane();
+        lbName.setStyle(s.clientLB);
+        lbAddress.setStyle(s.clientLB);
+        lbCity.setStyle(s.clientLB);
+        lbZip.setStyle(s.clientLB);
+        lbName = new Label("Client Name");
+    	lbAddress = new Label("Address");
+    	lbCity = new Label("City");
+    	
+    	vboxi.getChildren().addAll(lbName, lbAddress, lbCity, lbState);
+    	return vboxi;
+    }
+    /*
+     * This function will display the client information for the client page
+     */
+    private VBox getClientInfotx() {
+    	VBox vboxi = new VBox();
+    	TextField txName = new TextField();
+    	TextField txAddress = new TextField();
+    	TextField txCity = new TextField();
+    	vboxi.getChildren().addAll(txName, txAddress, txCity, getClientInfotxZip());
+    	return vboxi;
+    }   
+    /*
+     * This function will display the State and zip code information for the client page
+     */
+    private HBox getClientInfotxZip() {
+    	HBox hboxi = new HBox();
+    	lbZip = new Label("Zip");
+    	ComboBox<String> cbState = new ComboBox();
+    	txZip = new TextField();
+		cbState.getItems().addAll("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL",
+				"IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH",
+				"NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT",
+				"VA", "WA", "WV", "WI", "WY");
+    	hboxi.getChildren().addAll(cbState, lbZip,txZip);
+    	return hboxi;
+    } 
+    /**
+     * This is a function used to display the header section and the TextArea section for the result.
+     * @return
+     */
+    private VBox getViewLBs() {
+    	VBox vboxlb = new VBox();
+    	vboxlb.setAlignment(Pos.CENTER);
+    	vboxlb.getChildren().addAll(getViewLabel(), getTextAreaOne());
+    	return vboxlb;
+    }
+    /**
+     * This is a header section for the shipment view page. This header displays the names of the fields
+     * @return
+     */
+	private HBox getViewLabel() {
+		HBox hboxv = new HBox();
+		hboxv.setAlignment(Pos.CENTER);
+		hboxv.setPadding(new Insets(3, 20, 3, 20));
+		hboxv.setSpacing(150);
+		hboxv.setPrefWidth(700);
+		hboxv.setMaxWidth(900);
+		hboxv.setStyle("-fx-background-color: white; -fx-border-color: black");
+		Label name = new Label("Client Name");
+		Label Address = new Label("Address");
+		Label City = new Label("City");
+		Label State = new Label("State");
+		Label Zip = new Label("Zip Code");
+//		name.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
+//		Address.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
+//		City.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
+//		State.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
+		hboxv.getChildren().addAll(name, Address, City, State,Zip);
+		return hboxv;
+	}
+	
+	/**
+	 * This TextArea will display the output or result for the shipment information 
+	 * it will allow users to view a summary of the shipments. Users will not be able to 
+	 * change the shipment information from this shipment view.
+	 * @return
+	 */
+	private ScrollPane getTextAreaOne() {
+		ScrollPane box = new ScrollPane();
+		box.setFitToWidth(true);
+		box.setStyle("-fx-background-color: white; -fx-border-color: black"); 
+		//texReaOne = new TextArea();
+		//texReaOne.setStyle("-fx-border-color: black");
+		//texReaOne.setFont(new Font("Time New Roman", 10));
+		//texReaOne.setEditable(false);
+		//texReaOne.setWrapText(true);
+		//texReaOne.setPrefSize(900, 500);
+		
+		 GridPane gpane = new GridPane(); 
+		 gpane.setPadding(new Insets(2,20,2,20));
+		 gpane.setAlignment(Pos.TOP_CENTER); 
+		 gpane.setHgap(110);
+		 gpane.setVgap(5);
+		 		 
+		 int row = 1; 
+		 int i =0; 
+		 while(i < viewAllClient.getName().size()) {
+			 Label gridName = new Label (String.valueOf(viewAllClient.getName().get(i))); 
+			 Label gridAdress = new Label (String.valueOf(viewAllClient.getAddress1().get(i)) + " " +
+			 String.valueOf(viewAllClient.getAddress2().get(i))); 
+			 Label gridCity = new Label(String.valueOf(viewAllClient.getCity().get(i))); 
+			 Label gridState = new Label(String.valueOf(viewAllClient.getState().get(i))); 
+			 Label gridZip = new Label(String.valueOf(viewAllClient.getZip().get(i))); 
+			 
+			 gpane.add(gridName,  0,  row);
+			 gpane.add(gridAdress, 1, row); 
+			 gpane.add(gridCity,  2,  row);
+			 gpane.add(gridState,  3,  row);
+			 gpane.add(gridZip,  4,  row);
+			 
+			 row++; 
+			 i++; 
+		 }
+		
+		 box.setContent(gpane);
+		return box;
+	}
+  
 } //End Subclass ClientsPage
--- ChamplainAirFreight/src/main/java/edu/cco/ChamplainAirFreight/ShipmentsPage.java
+++ ChamplainAirFreight/src/main/java/edu/cco/ChamplainAirFreight/ShipmentsPage.java
@@ -1,10 +1,17 @@
 package edu.cco.ChamplainAirFreight;
 
 import java.util.Arrays;
+
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -21,12 +28,26 @@ import javafx.scene.text.Text;
  */
 //Begin Subclass ShipmentsPage
 public class ShipmentsPage {
-
-    //variables
-    BorderPane bPane = new BorderPane();
-
     //classes
     Styles s = new Styles();
+	static TextArea texReaOne = new TextArea();
+    
+	// Client address labels
+	static Label lbShipmentVolume = new Label("Shipment Volume");
+	static Label lbShipmentWeight = new Label("Shipment Weight");
+	static Label lbShipmentStartDate = new Label("Shipment Start Date");
+	static Label lbShipmentEndDate  = new Label("Shipment End Date");
+	static Label lbShipmentNotes = new Label("Shipment Notes ");
+
+	static TextField txShipmentVolume;
+	static TextField txShipmentWeight;
+	static TextField txShipmentStartDate;
+	static TextField txShipmentEndDate;
+
+	static TextArea texRea;
+	
+    //variables
+    BorderPane bPane = new BorderPane();
 
     /**
      * Constructor - pulls the border pane from CAF (main page)
@@ -73,13 +94,20 @@ public class ShipmentsPage {
         //add titles to titlebox. 
         titleBox.getChildren().addAll(title, instruct);
 
+        //creating center box to add client information - Pierre
+        HBox centerBox = new HBox();
+        centerBox.setAlignment(Pos.CENTER_LEFT);
+        centerBox.setMinHeight(300);
+        centerBox.setStyle("-fx-background-color: white");
+        centerBox.getChildren().addAll();
+      
         //create button HBox:
         HBox buttonBox = new HBox();
         buttonBox.setAlignment(Pos.CENTER);
         buttonBox.setSpacing(20);
 
         //make buttons
-        Button btnView = new Button("View Shipment");
+        Button btnView = new Button("View");
         Button btnAdd = new Button("Add Shipment");
         Button btnEdit = new Button("Edit Shipment");
         Button btnDelete = new Button("Delete Shipment");
@@ -105,18 +133,18 @@ public class ShipmentsPage {
 
         //add title, center, and buttons to shipment pane:
         box.setTop(titleBox);
-        box.setCenter(viewShipments()); //call a method to show db of shipments  
+        box.setCenter(centerBox); //call a method to show db of shipments  
         box.setBottom(buttonBox);
 
         //add actionables to change the setCenter based on button responses:
         btnView.setOnAction(e -> {
-            box.setCenter(viewShipments());
+        	box.setCenter(getViewLBs());
         });
-        btnAdd.setOnAction(e -> {
-            //box.setCenter(addShipment()); 
+        btnAdd.setOnAction(e -> {              	 
+        	 box.setCenter(getClientLBs() );
         });
         btnEdit.setOnAction(e -> {
-
+        	 box.setCenter(getClientLBs() );
         });
         btnDelete.setOnAction(e -> {
 
@@ -130,7 +158,7 @@ public class ShipmentsPage {
         btnExit.setOnAction(e -> {
             //clear whatever actions doing
             //return to just the viewShipments page
-            box.setCenter(viewShipments());
+            box.setCenter(addShipments());
         });
 
         return box;
@@ -142,11 +170,111 @@ public class ShipmentsPage {
      *
      * @return
      */
-    private ScrollPane viewShipments() {
+    private ScrollPane addShipments() {
         ScrollPane chart = new ScrollPane();
         chart.setMinHeight(360);
         chart.setMaxHeight(360);
 
         return chart;
     }
+  // 
+    /**
+     * This is a function used to display the header section and the TextArea section for the result.
+     * @return
+     */
+    private HBox getClientLBs() {
+    	HBox hboxlb = new HBox();
+    	hboxlb.setMinHeight(300);
+    	hboxlb.setStyle("-fx-background-color: white");
+    	hboxlb.setAlignment(Pos.CENTER_LEFT);
+    	hboxlb.getChildren().addAll( getShipInfolb(), getClientInfotx() );
+    	return hboxlb;
+    }   
+    /**
+     * This function displays the labels for the shipments.
+     * @return
+     */
+    private VBox getShipInfolb() {
+    	VBox vboxi = new VBox();
+		Label lbShipmentVolume = new Label("Shipment Volume");
+		Label lbShipmentWeight = new Label("Shipment Weight");
+		Label lbShipmentStartDate = new Label("Shipment Start Date");
+		Label lbShipmentEndDate = new Label("Shipment End Date");
+		Label lbShipmentNotes = new Label("Shipment Notes");
+    	vboxi.getChildren().addAll(lbShipmentVolume, lbShipmentWeight, lbShipmentStartDate,
+    			lbShipmentEndDate, lbShipmentNotes);
+    	return vboxi;
+    }
+    /**
+     * This function displays the text fields. User's will be able to enter information in the textfields
+     * @return
+     */
+    private VBox getClientInfotx() {
+    	VBox vboxi = new VBox();
+    	TextField txShipmentVolume = new TextField();
+    	TextField txShipmentWeight = new TextField();
+    	TextField txShipmentStartDate = new TextField();
+    	TextField txShipmentEndDate = new TextField();
+        texRea = new TextArea("");
+        texRea = new TextArea();
+        texRea.setStyle("-fx-border-color: black");
+        texRea.setFont(new Font("Time New Roman", 10));
+        texRea.setEditable(true);
+        texRea.setWrapText(true);
+        texRea.setPrefSize(600, 200);
+    	vboxi.getChildren().addAll(txShipmentVolume, txShipmentWeight, txShipmentStartDate, 
+    			txShipmentEndDate, texRea);
+    	return vboxi;
+    }
+    /**
+     * This is a function used to display the header section and the TextArea section for the result.
+     * @return
+     */
+    private VBox getViewLBs() {
+    	VBox vboxlb = new VBox();
+    	vboxlb.setAlignment(Pos.CENTER);
+    	vboxlb.getChildren().addAll(getViewLabel(), getTextAreaOne());
+    	return vboxlb;
+    }
+    /**
+     * This is a header section for the shipment view page. This header displays the names of the fields
+     * @return
+     */
+	private HBox getViewLabel() {
+		HBox hboxv = new HBox();
+		hboxv.setAlignment(Pos.CENTER);
+		hboxv.setPadding(new Insets(3, 20, 3, 20));
+		hboxv.setSpacing(150);
+		hboxv.setPrefWidth(700);
+		hboxv.setMaxWidth(900);
+		hboxv.setStyle("-fx-background-color: white; -fx-border-color: black");
+		Label name = new Label("Volume");
+		Label Address = new Label("Start Date");
+		Label City = new Label("End Date");
+		Label State = new Label("State");
+//		name.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
+//		Address.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
+//		City.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
+//		State.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
+		hboxv.getChildren().addAll(name, Address, City, State);
+		return hboxv;
+	}
+	/**
+	 * This TextArea will display the output or result for the shipment information 
+	 * it will allow users to view a summary of the shipments. Users will not be able to 
+	 * change the shipment information from this shipment view.
+	 * @return
+	 */
+	private HBox getTextAreaOne() {
+		HBox hboxt = new HBox();
+		texReaOne = new TextArea();
+		hboxt.setAlignment(Pos.CENTER);
+		texReaOne.setStyle("-fx-border-color: black");
+		texReaOne.setFont(new Font("Time New Roman", 10));
+		texReaOne.setEditable(false);
+		texReaOne.setWrapText(true);
+		texReaOne.setPrefSize(900, 500);
+		hboxt.getChildren().addAll(texReaOne);
+		return hboxt;
+	}
 } //End Subclass ShipmentsPage
