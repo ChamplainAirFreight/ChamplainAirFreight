dff14aa517706b117a2414be44a81e34cb11ff43
Kelly May <kelly.may@mymail.champlain.edu>
2020-11-24 12:41:58 -0500

    added view and view selected to aircraft page

--- src/main/java/edu/cco/ChamplainAirFreight/AircraftPage.java
+++ src/main/java/edu/cco/ChamplainAirFreight/AircraftPage.java
@@ -2,6 +2,9 @@ package edu.cco.ChamplainAirFreight;
 
 import java.util.Arrays;
 
+import edu.cco.ChamplainAirFreight.Database.Aircraft.DBViewAllAircraft;
+import edu.cco.ChamplainAirFreight.Database.Aircraft.DBViewSelectAircraft;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -49,6 +52,15 @@ public class AircraftPage {
 	static TextField txPayload ;
 	static TextField txLoadVolume;
 	
+	//make buttons
+    Button btnView = new Button("View");
+    Button btnAdd = new Button("Add Flight");
+    Button btnEdit = new Button("Edit Flight");
+    Button btnDelete = new Button("Delete Flight");
+    Button btnEnter = new Button("Enter");
+    Button btnCancel = new Button("Cancel");
+    Button btnExit = new Button("Exit");
+	
 /*	  = new Label("AirCraft Make");
 	  = new Label("AirCraft Model");
 	  = new Label("AirCraft Range");
@@ -59,6 +71,7 @@ public class AircraftPage {
 
     //classes
     Styles s = new Styles();
+    DBViewAllAircraft viewAllAir = new DBViewAllAircraft(); 
 
     /**
      * Constructor - pulls the border pane from CAF (main page)
@@ -92,33 +105,24 @@ public class AircraftPage {
         VBox titleBox = new VBox();
         titleBox.setAlignment(Pos.CENTER);
         titleBox.setSpacing(10);
-     //   Text title = new Text("Model");
-        Text instruct = new Text("Aircraft:");
+        Text title = new Text("Aircraft");
+        Text instruct = new Text("View Aircraft Information Below:");
         //style text
-//        title.setFill(Color.DARKBLUE);
-//        title.setStrokeWidth(2);
-//        title.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 25));
+        title.setFill(Color.DARKBLUE);
+        title.setStrokeWidth(2);
+        title.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 25));
 
         instruct.setFill(Color.BLUE);
         instruct.setStrokeWidth(2);
         instruct.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 20));
         //add titles to titlebox. 
-        titleBox.getChildren().addAll(instruct);//title, 
+        titleBox.getChildren().addAll(title, instruct); 
 
         //create button HBox:
         HBox buttonBox = new HBox();
         buttonBox.setAlignment(Pos.CENTER);
         buttonBox.setPadding(new Insets(20,0,0,0));
         buttonBox.setSpacing(20);
-
-        //make buttons
-        Button btnView = new Button("View");
-        Button btnAdd = new Button("Add Flight");
-        Button btnEdit = new Button("Edit Flight");
-        Button btnDelete = new Button("Delete Flight");
-        Button btnEnter = new Button("Enter");
-        Button btnCancel = new Button("Cancel");
-        Button btnExit = new Button("Exit");
 
         //style buttons
         Arrays.asList(btnView, btnAdd, btnEdit, btnDelete, btnEnter,
@@ -138,7 +142,7 @@ public class AircraftPage {
 
         //add title, center, and buttons to Aircraft pane:
         box.setTop(titleBox);
-        box.setCenter(craftView()); //call a method to show db of flights  
+        box.setCenter(getViewSelected()); //call a method to show db of flights  
         box.setBottom(buttonBox);
 
         //add actionables to change the setCenter based on button responses:
@@ -163,26 +167,13 @@ public class AircraftPage {
         btnExit.setOnAction(e -> {
             //clear whatever actions doing
             //return to just the viewFlights page
-            box.setCenter(craftView());
+            box.setCenter(getViewSelected());
         });
 
         return box;
     }
 
-    /**
-     * viewFlights - generates a chart of the Aircraft information, which can be
-     * added to flights() will have to call the database Aircraft table
-     *
-     * @return
-     */
-    private ScrollPane craftView() {
-        ScrollPane chart = new ScrollPane();
-        chart.setMinHeight(360);
-        chart.setMaxHeight(360);
-
-        return chart;
-    }
-    /**
+     /**
      * This is a function used to display the header section and the TextArea section for the result.
      * @return
      */
@@ -190,7 +181,7 @@ public class AircraftPage {
     	HBox hboxlb = new HBox();
     	hboxlb.setMinHeight(300);
     	hboxlb.setStyle("-fx-background-color: white");
-    	hboxlb.setAlignment(Pos.CENTER_LEFT);
+    	hboxlb.setAlignment(Pos.CENTER);
     	hboxlb.getChildren().addAll(getCraftInfolb(),getCraftlInfotx());
     	return hboxlb;
     }   
@@ -208,20 +199,11 @@ public class AircraftPage {
         lbtxPayload = new Label("Payload");
         lbtxLoadVolume = new Label("Load Volume");
         //Adding Styles
-        lbcraftMake.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 15));
-        lbcraftModel.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 15));
-        lbcraftRang.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 15));
-        lbcraftRC.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 15));
-        lbtxPayload.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 15));
-        lbtxLoadVolume.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 15));
-        lbcraftMake.setStyle(s.LBTextColor);
-        lbcraftMake.setStyle(s.LBTextColor);
-        lbcraftModel.setStyle(s.LBTextColor);
-        lbcraftRang.setStyle(s.LBTextColor);
-        lbcraftRC.setStyle(s.LBTextColor);
-        lbtxPayload.setStyle(s.LBTextColor);
-        lbtxLoadVolume.setStyle(s.LBTextColor);
-    	vboxi.getChildren().addAll(lbcraftMake, lbcraftModel, lbcraftRang, lbcraftRC,
+        Arrays.asList(lbcraftMake, lbcraftModel, lbcraftRang, lbcraftRC, lbtxPayload, lbtxLoadVolume).forEach((b)->{
+        	b.setStyle(s.LBTextColor);
+        	b.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 15)); 
+        }); 
+        vboxi.getChildren().addAll(lbcraftMake, lbcraftModel, lbcraftRang, lbcraftRC,
     			lbtxPayload, lbtxLoadVolume);
     	return vboxi;
     }
@@ -260,11 +242,13 @@ public class AircraftPage {
 	private HBox getAircraftLabel() {
 		HBox hboxv = new HBox();
 		hboxv.setAlignment(Pos.CENTER);
-		hboxv.setPadding(new Insets(3, 20, 3, 20));
-		hboxv.setSpacing(150);
+		hboxv.setPadding(new Insets(2, 20, 2, 20));
+		hboxv.setSpacing(70);
 		hboxv.setPrefWidth(700);
 		hboxv.setMaxWidth(900);
 		hboxv.setStyle("-fx-background-color: white; -fx-border-color: black");
+		Label lbcraftID = new Label("Aircraft ID"); 
+		Label lbStatusID = new Label("Status"); 
 		 lbcraftMake = new Label("AirCraft Make");
 		 lbcraftModel = new Label("AirCraft Model");
 		 lbcraftRang = new Label("AirCraft Range");
@@ -273,7 +257,7 @@ public class AircraftPage {
 //		Address.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
 //		City.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
 //		State.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 10));
-		hboxv.getChildren().addAll(lbcraftMake, lbcraftModel, lbcraftRang, lbcraftRC);
+		hboxv.getChildren().addAll(lbcraftID, lbStatusID, lbcraftMake, lbcraftModel, lbcraftRang, lbcraftRC);
 		return hboxv;
 	}
 
@@ -283,17 +267,128 @@ public class AircraftPage {
 	 * change the Aircraft information from this Aircraft view.
 	 * @return
 	 */
-	private HBox getTextAreaOne() {
-		HBox hboxt = new HBox();
-		texReaOne = new TextArea();
-		hboxt.setAlignment(Pos.CENTER);
-		texReaOne.setStyle("-fx-border-color: black");
-		texReaOne.setFont(new Font("Time New Roman", 10));
-		texReaOne.setEditable(false);
-		texReaOne.setWrapText(true);
-		texReaOne.setPrefSize(900, 500);
-		hboxt.getChildren().addAll(texReaOne);
-		return hboxt;
+	private ScrollPane getTextAreaOne() {
+		ScrollPane box = new ScrollPane(); 
+		box.setStyle("-fx-border-color: black");
+		box.setFitToWidth(true);
+		
+		GridPane gpane = new GridPane();
+		gpane.setStyle("-fx-background-color: white; -fx-border-color: black");
+		gpane.setAlignment(Pos.TOP_CENTER);
+		gpane.setPadding(new Insets(2,20,2,20)); 
+		gpane.setHgap(90);
+		gpane.setVgap(5);
+		
+		// dynamically add values from the database to the gridpane table 
+				int row = 1; 
+				 int i =0; 
+				 while(i < viewAllAir.getAircraftID().size()) {
+					 Label gridAirID = new Label (String.valueOf(viewAllAir.getAircraftID().get(i))); 
+					 Label gridStatusID = new Label (String.valueOf(viewAllAir.getAircraftStatusID().get(i))); 
+					 Label gridMake = new Label(String.valueOf(viewAllAir.getAircraftMake().get(i))); 
+					 Label gridModel = new Label(String.valueOf(viewAllAir.getAircraftModel().get(i))); 
+					 Label gridRange = new Label(String.valueOf(viewAllAir.getAircraftRange().get(i))); 
+					 Label gridRangeClass = new Label(String.valueOf(viewAllAir.getAircraftRangeClass().get(i))); 
+					 
+					 
+					 gpane.add(gridAirID,  0,  row);
+					 gpane.add(gridStatusID, 1, row); 
+					 gpane.add(gridMake,  2,  row);
+					 gpane.add(gridModel,  3,  row);
+					 gpane.add(gridRange,  4,  row);
+					 gpane.add(gridRangeClass,  5,  row);
+									 
+					 
+					 row++; 
+					 i++; 
+				 }
+				
+				box.setContent(gpane); 
+				return box;
+	}
+	
+	/**
+	 * getViewSelected - the initial pane for the viewAircraft Page, 
+	 * will house a feature to view a selected aircraft based on 
+	 * aircraft ID
+	 * @return
+	 */
+	private VBox getViewSelected(){
+		DBViewAllAircraft all  = new DBViewAllAircraft(); 
+		VBox centerBox = new VBox(); 
+		 centerBox.setAlignment(Pos.TOP_CENTER);
+		 centerBox.setMinHeight(300);
+		 centerBox.setStyle("-fx-background-color: white");
+		 
+		// add title and subtitle instructions 
+		    Text title = new Text("View Selected Aircraft"); 
+		    Text instructions = new Text("Use the scroll bar to select an AircraftID, then click SEARCH. \n"
+		    		+ "This will allow you to view all client information for selected plane.");
+		    HBox selection = new HBox(); 
+		    selection.setAlignment(Pos.CENTER);
+		    ComboBox airSelect = new ComboBox(FXCollections.observableArrayList(all.getAircraftID())); 
+		    airSelect.setVisibleRowCount(5);
+		    Button airSearch = new Button("Search"); 
+		    selection.getChildren().addAll(airSelect, airSearch); 
+		
+		    
+		  //grid of information
+		    GridPane grid = new GridPane(); 
+		    grid.setAlignment(Pos.CENTER); 
+		    Label lbID = new Label("Aircraft ID: "); 
+		    Label lbStatusID = new Label("Status ID: "); 
+		    Label lbMake = new Label("Make: "); 
+		    Label lbModel = new Label("Model: "); 
+		    Label lbRange = new Label("Range: "); 
+		    Label lbRangeClass = new Label("Range Classification: "); 
+		    Label lbPayload = new Label("Payload: "); 
+		    Label lbLoadVol = new Label("Load Volume: "); 
+		    
+		    Text txtID = new Text(); 
+		    Text txtStatusID = new Text(); 
+		    Text txtMake = new Text(); 
+		    Text txtModel = new Text(); 
+		    Text txtRange = new Text(); 
+		    Text txtRangeClass = new Text(); 
+		    Text txtPayload = new Text(); 
+		    Text txtLoadVol = new Text(); 
+		    
+		    grid.add(lbID, 0, 0);
+		    grid.add(lbStatusID, 0, 1);
+		    grid.add(lbMake, 0, 2);
+		    grid.add(lbModel, 0, 3);
+		    grid.add(lbRange,0,4); 
+		    grid.add(lbRangeClass,0,5);
+		    grid.add(lbPayload, 0, 6);
+		    grid.add(lbLoadVol, 0, 7);
+		    grid.add(txtID, 1, 0);
+		    grid.add(txtStatusID, 1, 1);
+		    grid.add(txtMake, 1, 2);
+		    grid.add(txtModel, 1, 3);
+		    grid.add(txtRange,1,4); 
+		    grid.add(txtRangeClass,1,5);
+		    grid.add(txtPayload, 1, 6);
+		    grid.add(txtLoadVol, 1, 7);
+		    
+		    airSearch.setOnAction(e->{
+		    	DBViewSelectAircraft view = new DBViewSelectAircraft(); 
+		    	int entry = 0; 
+		    	entry = Integer.parseInt(airSelect.getValue().toString());
+		    	view.viewSelected(entry);
+		    	
+		    	txtID.setText(Integer.toString(view.getAircraftID()));
+		    	txtStatusID.setText(Integer.toString(view.getStatusID()));
+		    	txtMake.setText(view.getMake());
+		    	txtModel.setText(view.getModel());
+		    	txtRange.setText(view.getRange());
+		    	txtRangeClass.setText(view.getRangeClass());
+		    	txtPayload.setText(view.getPayload().toString());
+		    	txtLoadVol.setText(view.getLoadVolume().toString());
+		    });
+		    
+		    centerBox.getChildren().addAll(title,instructions, selection, grid); 
+		    
+		return centerBox; 
 	}
 
 } //End Subclass Aircraft
